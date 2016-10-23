fd5d0bbd086f2de8fe091182b748c0333d222f33
Ttran96 <ttran96@gatech.edu>
2016-10-23 07:01:01 -0400

    Pulsify

--- app/src/main/java/com/pulsify/my/pulsify/MainActivity.java
+++ app/src/main/java/com/pulsify/my/pulsify/MainActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
@@ -19,10 +21,57 @@ import com.spotify.sdk.android.player.PlayerEvent;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
 
+import java.util.Random;
+
 import static android.R.id.message;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.harman.pulsesdk.DeviceModel;
+import com.harman.pulsesdk.ImplementPulseHandler;
+import com.harman.pulsesdk.PulseColor;
+import com.harman.pulsesdk.PulseHandlerInterface;
+import com.harman.pulsesdk.PulseNotifiedListener;
+import com.harman.pulsesdk.PulseThemePattern;
+
+import java.util.Random;
+
+import java.util.ArrayList;
+import java.lang.String;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements
         SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
+
+    Random randomPattern;
+    PulseColor bgColor;
+    PulseHandlerInterface pulseHandler;
+
+    protected static final int RESULT_SPEECH = 1;
+    private String s;
+    int randomNumber = 0, r, g, b;
+    private ImageButton btnSpeak;
+    private TextView txtText;
+
+    //--------------------------------------------------
 
     // TODO: Replace with your client ID
     private static final String CLIENT_ID = "c17d2870fc3c4b95a62998eae6c01aa9";
@@ -37,6 +86,7 @@ public class MainActivity extends AppCompatActivity implements
     private String mood;
     SpotifyModel spotifyModel = new SpotifyModel();
 
+    //--------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,7 +100,105 @@ public class MainActivity extends AppCompatActivity implements
 
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
 
+        //--------------------------------------------------
+
+        pulseHandler = new ImplementPulseHandler();
+
+        pulseHandler.registerPulseNotifiedListener(new PulseNotifiedListener() {
+            @Override
+            public void onConnectMasterDevice() {
+
+            }
+
+            @Override
+            public void onDisconnectMasterDevice() {
+
+            }
+
+            @Override
+            public void onRetBrightness(int i) {
+
+            }
+
+            @Override
+            public void onLEDPatternChanged(PulseThemePattern pulseThemePattern) {
+
+            }
+
+            @Override
+            public void onSoundEvent(int i) {
+
+            }
+
+            @Override
+            public void onRetCaptureColor(PulseColor pulseColor) {
+
+            }
+
+            @Override
+            public void onRetCaptureColor(byte b, byte b1, byte b2) {
+
+            }
+
+            @Override
+            public void onRetSetDeviceInfo(boolean b) {
+
+            }
+
+            @Override
+            public void onRetGetLEDPattern(PulseThemePattern pulseThemePattern) {
+
+            }
+
+            @Override
+            public void onRetRequestDeviceInfo(DeviceModel[] deviceModels) {
+
+            }
+
+            @Override
+            public void onRetSetLEDPattern(boolean b) {
+
+            }
+        });
+
+        pulseHandler.SetBrightness(10);
+        pulseHandler.ConnectMasterDevice(this);
+
+        //button Google
+        txtText = (TextView) findViewById(R.id.txtText);
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(
+                        RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                    txtText.setText("");
+                } catch (ActivityNotFoundException a) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Opps! Your device doesn't support Speech to Text",
+                            Toast.LENGTH_SHORT);
+                    t.show();
+                }
+            }
+        });
+
     }
+
+    //----------------------------------------------------------------------------------------------------
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    //----------------------------------------------------------------------------------------------------
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -76,6 +224,77 @@ public class MainActivity extends AppCompatActivity implements
                 });
             }
         }
+//--------------------------------------------------------------------------------------------------------------
+        switch (requestCode) {
+            case RESULT_SPEECH: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> text = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    txtText.setText(text.get(0));
+
+                    s = text.get(0);
+                    s = s.toLowerCase();
+
+                    switch (s) {
+                        case "excited": {
+                            pulseHandler.SetLEDPattern(PulseThemePattern.PulseTheme_Fire);
+                            Toast.makeText(getApplicationContext(), "LED pattern shuffled successfully to " + "FIRE", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        case "angry": {
+                            pulseHandler.SetLEDPattern(PulseThemePattern.PulseTheme_Canvas);
+                            Toast.makeText(getApplicationContext(), "LED pattern shuffled successfully to " + "CANVAS", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        case "relax": {
+                            pulseHandler.SetLEDPattern(PulseThemePattern.PulseTheme_Firefly);
+                            Toast.makeText(getApplicationContext(), "LED pattern shuffled successfully to " + "FIREFLY", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        case "crazy": {
+                            pulseHandler.SetLEDPattern(PulseThemePattern.PulseTheme_Firework);
+                            Toast.makeText(getApplicationContext(), "LED pattern shuffled successfully to " + "FIREWORK", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        case "sleepy": {
+                            pulseHandler.SetLEDPattern(PulseThemePattern.PulseTheme_Hourglass);
+                            Toast.makeText(getApplicationContext(), "LED pattern shuffled successfully to " + "HOURGLASS", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        case "thoughtful": {
+                            pulseHandler.SetLEDPattern(PulseThemePattern.PulseTheme_Rain);
+                            Toast.makeText(getApplicationContext(), "LED pattern shuffled successfully to " + "RAIN", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        case "jazzy": {
+                            pulseHandler.SetLEDPattern(PulseThemePattern.PulseTheme_Ripple);
+                            Toast.makeText(getApplicationContext(), "LED pattern shuffled successfully to " + "RIPPLE", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        case "sparkle": {
+                            pulseHandler.SetLEDPattern(PulseThemePattern.PulseTheme_Star);
+                            Toast.makeText(getApplicationContext(), "LED pattern shuffled successfully to " + "STAR", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        case "dance": {
+                            pulseHandler.SetLEDPattern(PulseThemePattern.PulseTheme_Traffic);
+                            Toast.makeText(getApplicationContext(), "LED pattern shuffled successfully to " + "TRAFFIC", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        case "do something": {
+                            pulseHandler.SetLEDPattern(PulseThemePattern.PulseTheme_Wave);
+                            Toast.makeText(getApplicationContext(), "LED pattern shuffled successfully to " + "WAVE", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                    }
+                    break;
+                }
+
+            }
+        }
+
     }
 
     @Override
